fafb99f58cd35cea320d933afa4f899883687a4c
leewooseong <sitpo96@naver.com>
2024-01-06 03:37:35 +0900

    Refactor: font 우선순위 관한 주석 추가

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 import { Inter, Lusitana } from 'next/font/google';
 
 // nextjs에서 제공하는 폰트를 이용해서 Font 객체를 받환받는다.
+// main font
 export const inter = Inter({ subsets: ['latin'] });
+// sub font
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
